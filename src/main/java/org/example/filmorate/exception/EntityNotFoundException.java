fe0ad732fe761791073861ba59ffaf45db7f94a0
Kuandyk Mukhamedali <alikuandyk26@gmail.com>
2024-09-16 16:30:46 +0500

    It was a pleasure

---
+++ src/main/java/org/example/filmorate/exception/EntityNotFoundException.java
@@ -0,0 +1,8 @@
+package org.example.filmorate.exception;
+
+public class EntityNotFoundException extends RuntimeException {
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+}
+
---
+++ src/main/java/org/example/filmorate/exception/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package org.example.filmorate.exception;
+
+import jakarta.validation.ValidationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler extends RuntimeException {
+    @ExceptionHandler(ValidationException.class)
+    public ResponseEntity<String> handleValidationException(ValidationException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<String> handleNotFoundException(EntityNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGeneralException(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Произошла ошибка: " + ex.getMessage());
+    }
+}
